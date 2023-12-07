0db22f02dc5f37cf8d9336129df66f7395654a72
TezukaKarlsefni <DjaluKun@gmail.com>
2023-12-07 12:57:07 +0700

    login-success

--- be/src/index.js
+++ be/src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 
 const app = express();
+const session = require('express-session');
+const passport = require('passport');
 const PORT = 3001;
-const getUser = require('./routes/getuserRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/api/v1/login', authRoutes);
 
 app.use((req, res, next) => {
   console.log(`${req.method}:${req.url}`);
---
+++ be/src/routes/authRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../../controllers/authController');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/user');
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+      User.findOne({ where: { username: username } })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'Incorrect username.' });
+          }
+          if (user.password !== password) {
+            return done(null, false, { message: 'Incorrect password.' });
+          }
+          return done(null, user);
+        })
+        .catch(err => done(err));
+    }
+  ));
+  
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    User.findByPk(id)
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+  
+  router.post('/login', authController.login);
+  router.get('/logout', authController.logout);
+  
+module.exports = router;
